734eb50ae3582097d878cefae6d6989bfca4be26
Bridget Amana <amanabridget03@gmail.com>
2026-04-23 13:53:50 +0100

    feat: update Projects and Setting components

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,35 @@
+import styles from "./Projects.module.css";
+
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+.board {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-lg);
+}
+
+.boardHeader {
+  display: flex;
+  align-items: end;
+  justify-content: space-between;
+  gap: var(--spacing-md);
+  flex-wrap: wrap;
+}
+
+.boardTitleGroup {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-xs);
+}
+
+.boardTitle {
+  font-size: var(--title);
+}
+
+.boardSubtitle {
+  font-size: var(--subtext);
+  color: var(--muted_text);
+}
+
+  );
 }
--- src/components/setting/Setting.jsx
+++ src/components/setting/Setting.jsx
@@ -1,5 +1,16 @@
+import styles from "./Setting.module.css"
+
 export default function Setting() {
   return (
-    <div>Setting</div>
-  )
+    <section>
+      <header className={styles.settingHeader}>
+        <div className={styles.settingTitleGroup}>
+          <h1 className={styles.settingTitle}>Setting </h1>
+          <p className={styles.settingSubtitle}>
+            Organize your deep work sessions
+          </p>
+        </div>
+      </header>
+    </section>
+  );
 }
